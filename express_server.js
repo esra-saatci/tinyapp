baf33187e873d09b24c9e2e6b411b8f8099503e1
esra-saatci <esra.saatci86@gmail.com>
2021-09-02 21:12:31 +0000

    Create a Registration Page

--- express_server.js
+++ express_server.js
@@ -110,6 +110,14 @@ app.post('/logout', (req, res) => {
   res.redirect("/urls");
 });
 
+// Create a route for registration page
+app.get('/register', (req, res) => {
+  const templateVars = {
+    username: req.cookies['username']
+  };
+  res.render('urls_register', templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
